4d7c26e0a3f16d23fc2c9e91eb990ce296054672
midhun raj <midhungreen@hotmail.com>
2019-10-12 14:10:34 +0400

    created UI

---
+++ AttendanceHander/AttendanceHander/Ribbon1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace AttendanceHander
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabAttendHandler = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.buttonProcess = this.Factory.CreateRibbonButton();
+            this.tabAttendHandler.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabAttendHandler
+            // 
+            this.tabAttendHandler.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tabAttendHandler.Groups.Add(this.group1);
+            this.tabAttendHandler.Label = "Attend Handler";
+            this.tabAttendHandler.Name = "tabAttendHandler";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.buttonProcess);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Label = "Process";
+            this.buttonProcess.Name = "buttonProcess";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tabAttendHandler);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tabAttendHandler.ResumeLayout(false);
+            this.tabAttendHandler.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabAttendHandler;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonProcess;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
