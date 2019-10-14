bcba581ad9cb74995a717b481c1e6c6782527bb7
midhun raj <midhungreen@hotmail.com>
2019-10-14 13:40:45 +0400

    changed the solution name

--- AttendanceHander/AttendanceHander/FormMain.Designer.cs
+++ AttendanceHander/AttendanceHander/FormMain.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 73);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add site Nos to current";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "FormMain";
             this.Text = "Attendance Hander";
             this.ResumeLayout(false);
@@ -42,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
